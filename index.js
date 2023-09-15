a6a4b6e2d032414eb40fa3836ced22d8c06fdc1a
leumasmelo <142456194+leumasmelo@users.noreply.github.com>
2023-09-15 19:14:33 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+Condicionais e objetos
+
+Exercícios
+
+A. Exercícios de interpretação de código
+
+1. a) O que o código acima faz? Explique em palavras simples.
+// Fornece os dados do nome do motorista, no caso a Alice, a idade, que é de 25 anos. Imprime se a pessoa é maior ou igual a 18 anos e tem carteira,pode dirigir
+//caso tenha menos de 18 anos, não pode dirigir.
+b) Quando a mensagem "Alice pode dirigir"" será impressa no console? E quando a mensagem "Alice não pode dirigir". Será impressa?
+//Quando constar que Alice é maior que 18 anos, será impressa no console. Alice não poderá dirigir quando constar que ela tem menos de 18 anos
+
+2. Leia o código a seguir:
+a) Fornece dados  de um animal "Leão" com parâmetro True (verdadeiro) para carnívoro ou parâmetro que ele não seja carnívoro.
+b) // Quando constar que Leão é um animal carnívoro, será impresso no console log. Caso não conste que o Leão não é carnívoro, mostrará mensagem que não é carnívoro.
+
