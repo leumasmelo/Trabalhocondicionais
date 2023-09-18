398d945519dbedfa67c00a39275ede26f0f2526e
leumasmelo <142456194+leumasmelo@users.noreply.github.com>
2023-09-18 19:25:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,4 +13,37 @@ b) Quando a mensagem "Alice pode dirigir"" será impressa no console? E quando a
 2. Leia o código a seguir:
 a) Fornece dados  de um animal "Leão" com parâmetro True (verdadeiro) para carnívoro ou parâmetro que ele não seja carnívoro.
 b) // Quando constar que Leão é um animal carnívoro, será impresso no console log. Caso não conste que o Leão não é carnívoro, mostrará mensagem que não é carnívoro.
+Questão 3
+Este código JavaScript realiza o seguinte:
 
+a) O código verifica se a variável "número" é maior que 5 e se é par ou ímpar, e então imprime mensagens no console com base nesses critérios.
+
+b) As mensagens impressas no console serão:
+   - "O número é maior que 5" (porque 10 é maior que 5).
+   - "O número é par" (porque 10 é divisível por 2, portanto é par).
+
+B.1. # Solicita ao usuário sua idade e armazena em uma variável idade idade = int(input("Digite sua idade:"))
+
+#Veridica se o usuário é maior ou igual a 18 anos
+If idade >=18:
+print("Você é maior de idade.")
+else:
+print("Você é menor de idade.")
+
+2. #Solicita ao usuário qual turno do dia ele estuda e armazena uma variável turno = input( "Qual turno do dia você estuda? Digite "M" para matutino, "T" para tarde e "N" para noturno.
+
+#Utiliza uma estrutura condicional para imprimir a mensagem apropriada com If turno == "M":
+print("Bom dia!")
+elif turno == "T":
+print("Boa tarde!")
+elif turno == "N":
+print("Boa noite!")
+else:
+print("Opção inválida. Por favor, digite 'M', 'T' ou 'N'.")
+3. Função para imprimir mensagem de acordo com o turno
+def imprimir _mensagem(turno):
+switch = {
+"M": "Bom dia!",
+"T": ""Boa tarde!",
+"N": "Boa noite!"
+}
